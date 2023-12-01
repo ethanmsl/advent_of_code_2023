0439328d7cf9a619c4b95c4971036de83f8ef5f5
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-12-01 17:40:28 +0100

    feat: working, accepted, tested solution to part1

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,8 +1,28 @@
 use crate::custom_error::AocError;
 
+/// Take first and last digit char (may be the same!)
+/// from each line to form a number.
+/// Return that sum.
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<u32, AocError> {
+    let mut nums = Vec::<u32>::new();
+
+    // lines
+    for ln in input.lines() {
+        // ascii digits
+        let chars: Vec<char> = ln.chars().filter(|c| c.is_ascii_digit()).collect();
+
+        // extracting, formatting, parsing, pushing
+        if let (Some(&first), Some(&last)) = (chars.first(), chars.last()) {
+            let str_num = format!("{}{}", first, last);
+            match str_num.parse::<u32>() {
+                Ok(num) => nums.push(num),
+                Err(_) => panic!("Could not parse: {} \nfrom line: {}", str_num, ln),
+            }
+        }
+    }
+
+    Ok(nums.into_iter().sum())
 }
 
 #[cfg(test)]
@@ -11,9 +31,11 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!(process(input)?, "");
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(process(input)?, 142);
         Ok(())
     }
 }
