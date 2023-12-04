077a2fb4c09f8fe40c252822a6f5a6eed0fb0904
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-12-04 13:16:55 -0800

    build(template): using `indoc` for input styling

--- day-02/src/part1_lib.rs
+++ day-02/src/part1_lib.rs
@@ -117,16 +117,19 @@ fn extract_data(hay: &str) -> (u64, Cubes) {
 #[cfg(test)]
 mod tests {
         use super::*;
+        use indoc::indoc;
 
         #[test]
         fn test_process_example() -> Result<()> {
                 tracing_subscriber::fmt::init();
 
-                let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+                let input = indoc! {"
+                        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+                        "};
                 let expected = 8;
                 assert_eq!(process(input)?, expected);
                 Ok(())
