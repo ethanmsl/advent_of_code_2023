24d1ff89a46d057f22e65fa900f52fd60305bc6a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-12-05 23:18:24 +0100

    skeleton: part2 day-04

--- day-04/src/part2_lib.rs
+++ day-04/src/part2_lib.rs
@@ -1,14 +1,117 @@
-//! Library code for Part 2 of Day 04 of Advent of Code 2023.
-//! `bin > part2.rs` will run this code along with conent of `input2.txt`
+//! Library code for Part 1 of Day 04 of Advent of Code 2023.
+//! `bin > part1.rs` will run this code along with conent of `input1.txt`
 
 use crate::custom_error::AocErrorDay04;
+use derive_more::{Constructor, IntoIterator};
 use miette::Result;
+use once_cell::sync::Lazy;
+use regex::Regex;
+use std::iter::FromIterator;
 use tracing::info;
 
-#[tracing::instrument]
-pub fn process(input: &str) -> Result<i64, AocErrorDay04> {
-        info!("Hiii. from  day-04 Part2! :)");
-        todo!("day 04 - Part 2");
+// // NOTE: these regex are overlapping
+// // If one decides to pick of elements of athe line for PERF
+// // then the borders will need to change
+// const CARD_NUM: &str = r"Card (\d+):"; // Card 1num:
+// const LEFT_NUMS: &str = r": (\d+) (\d+) (\d+) (\d+) (\d+) |"; // : 5nums |
+// const RIGHT_NUMS: &str = r"| (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)$"; // | 8 nums$
+
+// PERF:: for simple gobling up numbers in a row
+const NUM: &str = r"(\d+)";
+
+// #[tracing::instrument]
+pub fn process(input: &str) -> Result<u64, AocErrorDay04> {
+        todo!("copy of part1, not implemented yet");
+        info!("Hiii. from  day-04 Part1! :)");
+        let pile: CardPile = input.lines().collect();
+
+        Ok(pile.into_iter()
+                .map(|card| card.wining_haves_overlap())
+                .filter(|&n| n > 0)
+                .map(|n| 2u64.pow(n as u32 - 1))
+                .sum())
+}
+
+/// Represents a single scratch card.
+
+#[derive(Constructor, Debug, PartialEq, Eq)]
+struct ScratchCard {
+        id: u64,
+        // wins_arr: [u64; 5],
+        // haves_arr: [u64; 8],
+        wins_arr: [u64; 10],
+        haves_arr: [u64; 25],
+}
+
+impl ScratchCard {
+        /// Gives the number of winning nums
+        /// assumes wins_arr could be a set (i.e. ignoring non-uniqueness) while haves_arr may have
+        /// duplicates
+        fn wining_haves_overlap(&self) -> u64 {
+                self.haves_arr
+                        .iter()
+                        .filter(|&n| self.wins_arr.contains(n))
+                        .count() as u64
+        }
+        /// NOTE: the multiple refernces to splits is error prone
+        fn from_str(line: &str) -> Option<Self> {
+                // static RE_CARD: Lazy<Regex> = Lazy::new(|| Regex::new(CARD_NUM).unwrap());
+                // static RE_LEFT: Lazy<Regex> = Lazy::new(|| Regex::new(LEFT_NUMS).unwrap());
+                // static RE_RIGHT: Lazy<Regex> = Lazy::new(|| Regex::new(RIGHT_NUMS).unwrap());
+                static RE_NUM: Lazy<Regex> = Lazy::new(|| Regex::new(NUM).unwrap());
+
+                let ordered_nums: Vec<u64> = RE_NUM
+                        .find_iter(line)
+                        .map(|m| m.as_str().parse::<u64>().expect("parse failure"))
+                        .collect();
+
+                match ordered_nums.len() {
+                        // 14 => Some(ScratchCard::new(
+                        36 => Some(ScratchCard::new(
+                                ordered_nums[0],
+                                // ordered_nums[1..6]
+                                ordered_nums[1..11]
+                                        .try_into()
+                                        .expect("vec to array failure: A"),
+                                // ordered_nums[6..36]
+                                ordered_nums[11..36]
+                                        .try_into()
+                                        .expect("vec to array failure: B"),
+                        )),
+                        _ => None,
+                }
+        }
+}
+
+/// Represents a pile of (scratch) cards.
+/// (simple wrapper)
+#[derive(Debug, PartialEq, Eq, IntoIterator)]
+struct CardPile(Vec<ScratchCard>);
+
+impl CardPile {
+        fn new() -> Self {
+                Self(Vec::new())
+        }
+
+        /// Gives a mutable reference to the pile's cards
+        fn cards(&mut self) -> &mut Vec<ScratchCard> {
+                &mut self.0
+        }
+}
+
+impl<'a> FromIterator<&'a str> for CardPile {
+        fn from_iter<I: IntoIterator<Item = &'a str>>(iter: I) -> Self {
+                let mut pile = CardPile::new();
+
+                for item in iter {
+                        match ScratchCard::from_str(item) {
+                                Some(card) => pile.cards().push(card),
+                                None => panic!("Invalid card data on line {}", item),
+                        }
+                }
+
+                pile
+        }
 }
 
 #[cfg(test)]
@@ -16,22 +119,28 @@ mod tests {
         use super::*;
         use indoc::indoc;
 
+        // WARNING: the structure of the data is different from the main input
+        // the code is currently designed for the main input for perf reasons
+        // (and I just don't feel like doing multi pass or splitting)
         #[test]
         fn test_process_example() -> Result<()> {
                 tracing_subscriber::fmt::init();
 
                 let input = indoc! {"
-                        ...
-                        ...
-                        ...
+                        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
                 "};
-                let expected = todo!();
+                let expected = 30;
                 assert_eq!(process(input)?, expected);
                 Ok(())
         }
 
         // /// This test's expected value is to be populated after
-        // /// verification of solution.  
+        // /// verification of solution.
         // /// (useful for future refactors and perfs)
         // /// NOTE: `#[ignore]` is set for this test by default.
         // #[ignore]
