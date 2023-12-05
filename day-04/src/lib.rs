039a60b33e6735f9ffe4415f4a12ee79631136d1
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-12-05 20:58:55 +0100

    chore: cleanup

--- day-04/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Library code for Day 04 of Advent of Code 2023.
-//! This file is principally for collecting and ordering code.
-//! And the architecture of the crate will be hierarchical about `part1_lib.rs` and part2_lib.rs`.
-//! (With redundancy common, and by design, between those two files.)
-
-pub mod custom_error;
-
-pub mod part1_lib;
-pub mod part2_lib;
