ca235688e51554547303e98270d4f067c96091f5
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-12-11 03:43:16 +0100

    feat: input parsing working

--- day-05/src/part1_lib.rs
+++ day-05/src/part1_lib.rs
@@ -5,16 +5,20 @@
 use crate::custom_error::AocErrorDay05;
 use core::cell::OnceCell;
 use derive_more::{Constructor, IntoIterator};
+use itertools::Itertools;
 use miette::Result;
 use once_cell::sync::Lazy;
 use regex::Regex;
-use tracing::info;
+use std::ops::Range;
+use tracing::{debug, info, trace};
 
 // Capture Patterns for Regex generation
-static RE_SEEDS: Lazy<Regex> = Lazy::new(|| Regex::new(r"seeds: (.*)$").unwrap());
+static RE_SEEDS: Lazy<Regex> = Lazy::new(|| Regex::new(r"seeds: (?<seednumbers>.*)$").unwrap());
 static RE_NUM: Lazy<Regex> = Lazy::new(|| Regex::new(r"\d+").unwrap());
-static RE_A_TO_B: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\w+)-to-(\w+):").unwrap());
-static RE_VAL_MAP: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d+) (\d+) (\d+)").unwrap());
+static RE_A_TO_B: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"(?<input>\w+)-to-(?<output>\w+) map:").unwrap());
+static RE_VAL_MAP: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"^(?<outstart>\d+) (?<instart>\d+) (?<length>\d+)").unwrap());
 
 /// Return lowest "seed" to "location" mapping's location value.
 /// Parse seeds, maps kinds, and value map ranges.
@@ -22,7 +26,7 @@ static RE_VAL_MAP: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d+) (\d+) (\d+)").
 /// ## NOTES:
 /// - Values are approaching u32 MAX at least.
 ///   - u32::MAX ==              4_294_967_295
-///   - u64::MAX == 18_446_744_073_709_551_615
+///   - i64::MAX == 18_446_744_073_709_551_615
 ///   - sample_input_val ==      2_906_633_798
 /// - Part1 mappings appear to be ordered.
 ///   - e.g. a to b, b to c, c to d
@@ -61,14 +65,18 @@ static RE_VAL_MAP: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d+) (\d+) (\d+)").
 // #[tracing::instrument]
 pub fn process(input: &str) -> Result<i64, AocErrorDay05> {
         info!("Hiii. from  day-05 Part1! :)");
-        let mut it_inp = input.lines();
-        let first_line = it_inp.next().expect("empty input");
+        // splitting input along blank lines (blank lines discarded)
+        let mut it_chunk = input.split("\n\n");
+        let first_line = it_chunk.next().expect("empty input");
 
         let seeds: Vec<DynThings> = read_seeds(first_line).ok_or(AocErrorDay05::SeedsParse(
                 "Failed to parse seeds".to_string(),
         ))?;
-        info!("seeds: {:?}", seeds);
-        // let maps: Maps = it_inp.collect();
+        debug!("seeds: {:?}", seeds);
+
+        let maps: Vec<Map> = it_chunk.map(|chunk| Map::from_str(chunk)).collect();
+        // trace!("maps: {:?}", maps);
+
         //
         // seeds.iter().map(|seed| maps.propogate_complete(seed) ).min()
         // let seeds_w = maps.propogate_complete(seeds);
@@ -83,9 +91,108 @@ fn read_seeds(line: &str) -> Option<Vec<DynThings>> {
 
         Some(RE_NUM
                 .find_iter(line)
-                .map(|m| m.as_str().parse::<u64>().expect("parse failure"))
+                .map(|m| m.as_str().parse::<i64>().expect("parse failure"))
                 .map(|val| DynThings::new(SEED.to_string(), val))
                 .collect())
+}
+
+/// input & output values and 'range-bumps' that may modify an object
+/// PERF: replace Strings with &str referencing static strings.
+#[derive(Debug, PartialEq, Eq, Constructor)]
+struct Map {
+        inp: String,
+        out: String,
+        rmaps: Vec<RangeBump>,
+}
+
+impl Map {
+        /// Passes a value in and passes it back, incremented if it matches any of the ranges.
+        /// With**out** validating input kind or relaying output kind.
+        fn val_only_passthrough(&self, val: i64) -> i64 {
+                self.rmaps
+                        .iter()
+                        .filter_map(|rmap| rmap.try_bump(val))
+                        .next()
+                        .unwrap_or(val)
+        }
+
+        // Populate a map from a contiguous chunk of map string data.
+        fn from_str(chunk: &str) -> Self {
+                let mut lines = chunk.lines();
+                let mut rmaps: Vec<RangeBump> = Vec::new();
+                let first_line = lines.next().expect("empty chunk");
+                trace!("first_line: {:?}", first_line);
+
+                let caps = RE_A_TO_B.captures(first_line).expect("invalid map header");
+                let inp = caps
+                        .name("input")
+                        .expect("invalid 'input' map header")
+                        .as_str()
+                        .to_string();
+                let out = caps
+                        .name("output")
+                        .expect("invalid 'output' map header")
+                        .as_str()
+                        .to_string();
+                lines.for_each(|line| {
+                        let caps = RE_VAL_MAP.captures(line).expect("invalid map line");
+                        let in_start = caps
+                                .name("instart")
+                                .expect("in_start")
+                                .as_str()
+                                .parse::<i64>()
+                                .expect("instart parse failure");
+                        let out_start = caps
+                                .name("outstart")
+                                .expect("outstart")
+                                .as_str()
+                                .parse::<i64>()
+                                .expect("outstart parse failure");
+                        let length = caps
+                                .name("length")
+                                .expect("length")
+                                .as_str()
+                                .parse::<i64>()
+                                .expect("length parse failure");
+
+                        rmaps.push(RangeBump::new(
+                                (out_start - in_start),
+                                (in_start..in_start + length),
+                        ));
+                });
+
+                Self::new(inp, out, rmaps)
+        }
+}
+// let caps = re.captures(hay).unwrap();
+// assert_eq!(caps.get(0).unwrap().as_str(), "'Citizen Kane' (1941)");
+// assert_eq!(caps.name("title").unwrap().as_str(), "Citizen Kane");
+// assert_eq!(caps.name("year").unwrap().as_str(), "1941");
+// // You can also access the groups by name using the Index notation.
+// // Note that this will panic on an invalid group name. In this case,
+// // these accesses are always correct because the overall regex will
+// // only match when these capture groups match.
+// assert_eq!(&caps[0], "'Citizen Kane' (1941)");
+// assert_eq!(&caps["title"], "Citizen Kane");
+// assert_eq!(&caps["year"], "1941");
+
+/// A range of values and the amount they are bumped by.
+#[derive(Debug, PartialEq, Eq, Constructor)]
+struct RangeBump {
+        offset: i64,
+        range: Range<i64>,
+}
+
+impl RangeBump {
+        /// Returns true if the given value is in the range.
+        /// If so, the offset is applied to the value.
+        fn try_bump(&self, val: i64) -> Option<i64> {
+                if self.range.contains(&val) {
+                        Some(val + self.offset)
+                } else {
+                        None
+                }
+        }
 }
 
 /// Object with dynamic 'kind' indicated by a string and a positive value.
@@ -97,12 +204,9 @@ fn read_seeds(line: &str) -> Option<Vec<DynThings>> {
 #[derive(Debug, PartialEq, Eq, Constructor)]
 struct DynThings {
         kind: String,
-        val: u64,
+        val: i64,
 }
 
-// parser
-// // seeds:
-//
 #[cfg(test)]
 mod tests {
         use super::*;
@@ -110,7 +214,7 @@ mod tests {
 
         #[test]
         fn test_process_example() -> Result<()> {
-                tracing_subscriber::fmt::init();
+                tracing_subscriber::fmt::init(); // RUST_LOG=...
 
                 let input = indoc! {"
                         seeds: 79 14 55 13
