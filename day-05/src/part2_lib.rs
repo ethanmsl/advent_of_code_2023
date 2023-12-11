46dcd1f8d1d860398bf559405066f4ec7e4aa247
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-12-11 04:51:10 +0100

    feat: input accepted, added as test
    Excitement: the raw approach took a couple minutes.  The smarter
    approach (operating on and splitting and combining) ranges would likely
    get around this, but I'm still excited that there was a problem that
    took some time.  In particular, I look forward to trying par_iter with
    rayon in the naive solution.

--- day-05/src/part2_lib.rs
+++ day-05/src/part2_lib.rs
@@ -267,16 +267,16 @@ mod tests {
                 Ok(())
         }
 
-        // /// This test's expected value is to be populated after
-        // /// verification of solution.
-        // /// (useful for future refactors and perfs)
-        // /// NOTE: `#[ignore]` is set for this test by default.
-        // #[ignore]
-        // #[test]
-        // fn test_process_problem_input() -> Result<()> {
-        //         let file_input = include_str!("../input1.txt");
-        //         let expected = todo!();
-        //         assert_eq!(process(file_input)?, expected);
-        //         Ok(())
-        // }
+        /// This test's expected value is to be populated after
+        /// verification of solution.
+        /// (useful for future refactors and perfs)
+        /// NOTE: `#[ignore]` is set for this test by default.
+        #[ignore]
+        #[test]
+        fn test_process_problem_input() -> Result<()> {
+                let file_input = include_str!("../input1.txt");
+                let expected = 2520479;
+                assert_eq!(process(file_input)?, expected);
+                Ok(())
+        }
 }
