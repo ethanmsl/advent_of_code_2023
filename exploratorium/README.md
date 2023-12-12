f1ab81b506363e6436881a603743b870f12c6597
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-12-12 21:59:58 +0100

    skeleton: exploratorium, for future use

---
+++ exploratorium/README.md
@@ -0,0 +1,3 @@
+# Exploratorium / Zoo / Field Research
+
+This crate exists to explore different crate options. And to store useful, runnable, reference snippets of code.
---
+++ exploratorium/src/bin/regex.rs
@@ -0,0 +1,93 @@
+//! Library code for Part 1 of Day 05 of Advent of Code 2023.
+//! `bin > part1.rs` will run this code along with conent of `input1.txt`
+
+use anyhow::Result;
+use derive_more::Constructor;
+use once_cell::sync::Lazy;
+use regex::Regex;
+use tracing::{debug, info, trace};
+
+const TEXT: &str = indoc! {"
+        seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+"};
+
+// Capture Patterns for Regex generation
+const SEEDS_PAT: &str = r"seeds: (?<seednumbers>.*)$"
+const NUM_PAT: &str = r"\d+";
+const A_TO_B_PAT: &str = r"^(?<input>\d+) (?<output>\d+)$";
+const VAL_MAP_PAT: &str = r"^(?<outstart>\d+) (?<instart>\d+) (?<length>\d+)";
+
+static RE_SEEDS: Lazy<Regex> = Lazy::new(|| Regex::new(SEEDS_PATTERN).unwrap());
+static RE_NUM: Lazy<Regex> = Lazy::new(|| Regex::new(NUM_PAT).unwrap());
+static RE_A_TO_B: Lazy<Regex> =
+        Lazy::new(|| Regex::new(A_TO_B_PAT).unwrap());
+static RE_VAL_MAP: Lazy<Regex> =
+        Lazy::new(|| Regex::new(VAL_MAP_PAT).unwrap());
+
+
+/// Read a single line string and extract seed values.
+fn read_seeds(line: &str) -> Option<Vec<DynThings>> {
+        const SEED: &str = "seed";
+        let Some(_) = RE_SEEDS.captures(line) else {
+                return None;
+        };
+
+        Some(RE_NUM
+                .find_iter(line)
+                .map(|m| m.as_str().parse::<i64>().expect("parse failure"))
+                .map(|val| DynThings::new(SEED.to_string(), val))
+                .collect())
+}
+
+// Populate a map from a contiguous chunk of map string data.
+fn from_str(chunk: &str) -> () {
+        let mut lines = chunk.lines();
+        let mut rmaps: Vec<RangeBump> = Vec::new();
+        let first_line = lines.next().expect("empty chunk");
+        trace!("first_line: {:?}", first_line);
+
+        let caps = RE_A_TO_B.captures(first_line).expect("invalid map header");
+        let inp = caps
+                .name("input")
+                .expect("invalid 'input' map header")
+                .as_str()
+                .to_string();
+        let out = caps
+                .name("output")
+                .expect("invalid 'output' map header")
+                .as_str()
+                .to_string();
+
+        lines.for_each(|line| {
+                let caps = RE_VAL_MAP.captures(line).expect("invalid map line");
+                let in_start = caps
+                        .name("instart")
+                        .expect("in_start")
+                        .as_str()
+                        .parse::<i64>()
+                        .expect("instart parse failure");
+                let out_start = caps
+                        .name("outstart")
+                        .expect("outstart")
+                        .as_str()
+                        .parse::<i64>()
+                        .expect("outstart parse failure");
+                let length = caps
+                        .name("length")
+                        .expect("length")
+                        .as_str()
+                        .parse::<i64>()
+                        .expect("length parse failure");
+
+                rmaps.push(RangeBump::new(
+                        out_start - in_start,
+                        in_start..(in_start + length),
+                ));
+        });
+()
+
+}
